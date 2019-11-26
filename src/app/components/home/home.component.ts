64d806783222709f33daee7980f8e91064a02e16
akshatdb <akshatd4@gmail.com>
2019-11-26 09:16:35 +0000

    Added res data in table to debug

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataTableModel } from '../common/data-table/data-table.model';
+import { LoggerService } from 'src/app/services/logger.service';
 
 @Component({
   selector: 'app-home',
@@ -8,24 +9,42 @@ import { DataTableModel } from '../common/data-table/data-table.model';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private logger: LoggerService) { }
   data = [
-    {fieldname: 'Button', noofclicks: 4, route: '/form'},
-    {fieldname: 'Link', noofclicks: 1, route: '/form'},
-    {fieldname: 'Row', noofclicks: 2, route: '/form'},
-    {fieldname: 'Icon', noofclicks: 5, route: '/form'}
+    { fieldname: 'Button', noofclicks: 4, route: '/form' },
+    { fieldname: 'Link', noofclicks: 1, route: '/form' },
+    { fieldname: 'Row', noofclicks: 2, route: '/form' },
+    { fieldname: 'Icon', noofclicks: 5, route: '/form' }
   ];
-  tableModel:DataTableModel;  
+  deviceData = [
+
+  ]
+  tableModel: DataTableModel;
+  deviceDataModel: DataTableModel;
   ngOnInit() {
     this.tableModel = new DataTableModel({
       dataSource: this.data,
-      columns: ['fieldname', 'noofclicks','route'],
+      columns: ['fieldname', 'noofclicks', 'route'],
       columnDefs: [
-        {name: 'fieldname', header:'Field'},
-        {name: 'noofclicks', header:'No of clicks'},
-        {name: 'route', header: 'Route'}
+        { name: 'fieldname', header: 'Field' },
+        { name: 'noofclicks', header: 'No of clicks' },
+        { name: 'route', header: 'Route' }
       ]
     });
+    let info = this.logger.getInfo();
+    this.deviceData = [
+      { name: 'Operating System', value: info['operatingSystem'] },
+      { name: 'DeviceType', value: info['device'] },
+      { name: 'Resolution', value: info['resolution'] }
+    ]
+    this.deviceDataModel = new DataTableModel({
+      dataSource: this.deviceData,
+      columns: ['name', 'value'],
+      columnDefs: [
+        { name: 'name', header: 'Property' },
+        { name: 'value', header: 'Value' }
+      ]
+    })
   }
 
 }
